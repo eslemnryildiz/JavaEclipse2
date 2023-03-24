54608ca94dd86820dd4e4e6f754e0317d25e7f0e
Eslem <Eslem@DESKTOP-BJ9G8DU>
2023-03-24 13:14:34 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,41 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Product product1 = new Product();
+		product1.setName("Arçelik kahve makinesi");
+		product1.setGorselUrl("gorsel1.jpg");
+		product1.setIndirimOranı(5);
+		product1.setStok(6);
 
+		Product product2 = new Product();
+		product2.setName("Beko kahve makinesi");
+		product2.setGorselUrl("gorsel2.jpg");
+		product2.setIndirimOranı(7);
+		product2.setStok(3);
+
+		Product[] products = { product1, product2 };
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setCustomerNumber("12345");
+		individualCustomer.setFirstName("Eslem");
+		individualCustomer.setLastName("Yıldız");
+		individualCustomer.setId(1654);
+		individualCustomer.setPhone("0522222222");
+
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.setCompanyName("kodlama");
+		corporateCustomer.setCustomerNumber("15896");
+		corporateCustomer.setId(79564);
+		corporateCustomer.setPhone("0524665565");
+		corporateCustomer.setTaxNumber("55621656");
+
+		Customer[] customer = { individualCustomer, corporateCustomer };
+		for (Customer customers : customer) {
+			System.out.println(customers.getCustomerNumber());
+		}
 	}
 
 }
--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,40 @@
 
 public class Product {
+	private String name;
+	private int stok;
+	private String gorselUrl;
+	private int indirimOranı;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStok() {
+		return stok;
+	}
+
+	public void setStok(int stok) {
+		this.stok = stok;
+	}
+
+	public String getGorselUrl() {
+		return gorselUrl;
+	}
+
+	public void setGorselUrl(String gorselUrl) {
+		this.gorselUrl = gorselUrl;
+	}
+
+	public int getIndirimOranı() {
+		return indirimOranı;
+	}
+
+	public void setIndirimOranı(int indirimOranı) {
+		this.indirimOranı = indirimOranı;
+	}
 
 }
